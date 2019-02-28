55f04970a0521d26378ca65674ff289b5dd09d0a
Martin Güthler <mg@webfactory.de>
2019-02-28 16:46:22 +0100

    Add "setDoNotTrack", "trackPageView" and "enableLinkTracking" parameters to tracking code

--- Twig/Extension.php
+++ Twig/Extension.php
@@ -46,7 +46,9 @@ class Extension extends \Twig_Extension
 <!-- Piwik -->
 <script type="text/javascript">//<![CDATA[
 var _paq = (_paq||[]).concat({$paq});
-
+_paq.push(["setDoNotTrack", true]);
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
 (function() {
     var u=(("https:" == document.location.protocol) ? "https" : "http") + "://{$this->piwikHost}/";
     _paq.push(["setTrackerUrl", u+"{$this->trackerPath}"]);
