c9ef8d9a50303506bc5b381f7139858a236fb0ac
Martin Güthler <mg@webfactory.de>
2025-07-31 15:08:27 +0200

    Vermeide Deprecation
    ...durch Nutzung von `Symfony\Component\DependencyInjection\Extension\Extension` an Stelle von `Symfony\Component\HttpKernel\DependencyInjection\Extension`.

--- DependencyInjection/WebfactoryPiwikExtension.php
+++ DependencyInjection/WebfactoryPiwikExtension.php
@@ -4,8 +4,8 @@ namespace Webfactory\Bundle\PiwikBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class WebfactoryPiwikExtension extends Extension
 {
