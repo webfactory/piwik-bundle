e5927fbec46e64ed6ec74b88925883fada6f77f7
Matthias Pigulla <mp@webfactory.de>
2025-12-15 17:47:43 +0100

    Update PHPUnit to 10.x, clean up package structure, bump requirements

--- .github/workflows/lock-symfony-version.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cat <<< $(jq --arg version $VERSION '.require  |= with_entries(if ((.key|test("^symfony/monolog")|not) and (.key|test("^symfony/"))) then .value=$version else . end)' < composer.json) > composer.json
--- tests/Twig/ExtensionTest.php
+++ tests/Twig/ExtensionTest.php
@@ -1,38 +1,39 @@
 <?php
 
-namespace Twig;
+namespace Webfactory\Bundle\PiwikBundle\Tests\Twig;
 
 use PHPUnit\Framework\TestCase;
+use Twig\Extension\ExtensionInterface;
 use Webfactory\Bundle\PiwikBundle\Twig\Extension;
 
 class ExtensionTest extends TestCase
 {
-    public function testPiwikCodeReturnsNoScriptWhenDisabled()
+    public function testPiwikCodeReturnsNoScriptWhenDisabled(): void
     {
         $extension = new Extension(true, 1, '', false);
         self::assertStringNotContainsString('script', $extension->piwikCode());
     }
 
-    public function testPiwikCodeReturnsScript()
+    public function testPiwikCodeReturnsScript(): void
     {
         $extension = new Extension(false, 1, '', false);
         self::assertStringContainsString('script', $extension->piwikCode());
     }
 
-    public function testPiwikCodeContainsSiteId()
+    public function testPiwikCodeContainsSiteId(): void
     {
         $siteId = 1234;
         $extension = new Extension(false, $siteId, '', false);
         self::assertStringContainsString((string) $siteId, $extension->piwikCode());
     }
 
-    public function testPiwikCodeContainsApiURL()
+    public function testPiwikCodeContainsApiURL(): void
     {
         $extension = new Extension(false, 1, 'my.host', '/js/');
         self::assertStringContainsString('_paq.push([\'setAPIUrl\', u]);', $extension->piwikCode());
     }
 
-    public function testPiwikCodeDoesNotContainApiURL()
+    public function testPiwikCodeDoesNotContainApiURL(): void
     {
         $siteId = 1;
         $hostname = 'myHost.de';
@@ -41,46 +42,46 @@ class ExtensionTest extends TestCase
         self::assertStringNotContainsString('_paq.push([\'setAPIUrl\', u]);', $extension->piwikCode());
     }
 
-    public function testPiwikCodeContainsHostName()
+    public function testPiwikCodeContainsHostName(): void
     {
         $hostname = 'myHost.de';
         $extension = new Extension(false, 1, $hostname, false);
         self::assertStringContainsString($hostname, $extension->piwikCode());
     }
 
-    public function testPiwikOptOutCodeContainsHostName()
+    public function testPiwikOptOutCodeContainsHostName(): void
     {
         $hostname = 'myHost.de';
         $extension = new Extension(false, 1, $hostname, false);
         self::assertStringContainsString($hostname, $extension->piwikOptOutCode());
     }
 
-    public function testAdditionalApiCallsCanBeAdded()
+    public function testAdditionalApiCallsCanBeAdded(): void
     {
         $extension = new Extension(false, 1, 'my.host', false);
         $extension->piwikPush('foo', 'bar', 'baz');
         self::assertStringContainsString('["foo","bar","baz"]', $extension->piwikCode());
     }
 
-    public function testTrackPageViewEnabledByDefault()
+    public function testTrackPageViewEnabledByDefault(): void
     {
         $extension = new Extension(false, 1, 'my.host', false);
         self::assertStringContainsString('"trackPageView"', $extension->piwikCode());
     }
 
-    public function testCookiesCanBeDisabled()
+    public function testCookiesCanBeDisabled(): void
     {
         $extension = new Extension(false, 1, 'my.host', false);
         self::assertStringContainsString('"disableCookies"', $extension->piwikCode());
     }
 
-    public function testCookiesCanBeEnabled()
+    public function testCookiesCanBeEnabled(): void
     {
         $extension = new Extension(false, 1, 'my.host', false, false);
         self::assertStringNotContainsString('"disableCookies"', $extension->piwikCode());
     }
 
-    public function testTrackSiteSearchDisablesPageTracking()
+    public function testTrackSiteSearchDisablesPageTracking(): void
     {
         $extension = new Extension(false, 1, 'my.host', false);
         $extension->piwikPush('trackSiteSearch', 'Banana', 'Organic Food', 42);
@@ -90,9 +91,9 @@ class ExtensionTest extends TestCase
         self::assertStringNotContainsString('"trackPageView"', $code);
     }
 
-    public function testIsTwigExtension()
+    public function testIsTwigExtension(): void
     {
         $extension = new Extension(false, 1, '', false);
-        $this->assertInstanceOf(\Twig\Extension\ExtensionInterface::class, $extension);
+        $this->assertInstanceOf(ExtensionInterface::class, $extension);
     }
 }
